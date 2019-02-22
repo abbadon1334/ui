61809d506239327b20e53bf646c52d12e6e2b002
abbadon1334 <fdanti@gmail.com>
2019-02-22 18:25:40 +0200

    Add TableColumn NoValue to add a null value when is null

---
+++ src/TableColumn/NoValue.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright (c) 2019.
+ *
+ * Francesco "Abbadon1334" Danti <fdanti@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace atk4\ui\TableColumn;
+
+use atk4\ui\Exception;
+
+/**
+ * Class NoValue
+ *
+ * sometime we need null values in db
+ *
+ * when we display values we have holes
+ * with NoValue decorator we can show a display value for column null value
+ *
+ * @usage :
+ *
+ * $this->addField('field', [
+ *  [...]
+ *  'ui' => [
+ *          [...]
+ *          'table' => [
+ *              'NoValue', ' if empty display this value '
+ *          ]
+ *      ]
+ * ]);
+ *
+ *
+ * @package atk4\ui\TableColumn
+ */
+
+class NoValue extends Generic
+{
+    public $no_value = null;
+    
+    public function __construct($no_value = null)
+    {
+        $this->no_value = $no_value;
+    }
+    
+    public function getHTMLTags($row, $field)
+    {
+        $actualValue = $field->get();
+    
+        $value = !empty($actualValue) ? $actualValue : $this->no_value;
+        
+        return [$field->short_name => $value];
+    }
+}
